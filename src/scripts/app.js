8bb6ebb54fb74c9baf05ea73b9cd9f5b7fdf9ab8
Alba Raido <arubinat@uoc.edu>
2022-06-14 07:40:14 +0200

    prueba practica

---
+++ src/scripts/app.js
@@ -0,0 +1,29 @@
+
+const navToggle = document.querySelector(".toggle");
+const navMenu = document.querySelector(".nav-menu");
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if(navMenu.classList.contains("nav-menu_visible")) {
+        navToggle.setAttribute("aria-label", "Cerrar");
+    }else{
+        navToggle.setAttribute("aria-label", "Abrir");
+    }
+});
+
+
+const Top = document.getElementById('totop');
+window.onscroll = function() {
+  scrollFunction()
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    Top.classList.add('totop--show');
+  } else {
+    Top.classList.remove('totop--show');
+  }
+}
+Top.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
